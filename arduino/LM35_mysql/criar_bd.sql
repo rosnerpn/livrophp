702efaa6a670274b496d72036425d842eb49dce5
Rosner Nascimento <rosnerti@outlook.com>
2018-10-03 14:43:55 -0300

    removi as pasta incorretas

--- arduino/LM35_mysql/criar_bd.sql
+++
@@ -1,8 +0,0 @@
-﻿create database banco_arduino collate = utf8_general_ci;
-
-use banco_arduino;
-
-create table registrotemp(
-	id int(11) AUTO_INCREMENT PRIMARY KEY,
-	temperatura double,
-	data timestamp default current_timestamp);
