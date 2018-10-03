289a284985ff3c06b405f42696a5af7039adca0d
Rosner Nascimento <rosnerti@outlook.com>
2018-10-03 14:39:11 -0300

    Sensor de temperatura LM35 utilizado para verificar a temperatura na sala dos servidores e armazenar no banco de dados

---
+++ arduino/LM35_mysql/criar_bd.sql
@@ -0,0 +1,8 @@
+﻿create database banco_arduino collate = utf8_general_ci;
+
+use banco_arduino;
+
+create table registrotemp(
+	id int(11) AUTO_INCREMENT PRIMARY KEY,
+	temperatura double,
+	data timestamp default current_timestamp);
